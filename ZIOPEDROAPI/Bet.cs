278daeed668070414ec178dc0059a450f1fa7bde
ruben <ruben@RUBEN_PIRRERA>
2024-03-25 18:04:34 +0100

    Aggiungere i file di progetto.

---
+++ ZIOPEDROAPI/Bet.cs
@@ -0,0 +1,88 @@
+﻿namespace ZIOPEDROAPI
+{
+    public class Bet
+    {
+        public int IdTrain { get; set; }
+
+        public DateTime TimeStamp;
+        public int CodiceTreno;
+
+    }
+    public class Fermata
+    {
+        public string Id { get; set; }
+        private string stazione;
+        public string Stazione { get { return stazione; } set { if (value == null) stazione = string.Empty; else stazione = value; } }
+        private string provvedimenti;
+        public string Provvedimenti { get { return provvedimenti; } set { if (value == null) provvedimenti = string.Empty; else provvedimenti = value; } }
+        public long Programmata { get; set; }
+        public int Ritardo { get; set; }
+        public object Partenza_Teorica { get; set; }
+        public object Arrivo_Teorico { get; set; }
+        public object PartenzaReale { get; set; }
+        public object ArrivoReale { get; set; }
+        public int RitardoPartenza { get; set; }
+        public int RitardoArrivo { get; set; }
+    }
+
+    internal class Treno
+    {
+        public string NumeroTreno { get; set; }
+        public string OrigineZero { get; set; }
+        public string DestinazioneZero { get; set; }
+        public List<Fermata> Fermate { get; set; }
+
+        public DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+        public string DisplayTrainData()
+        {
+            string s = ($"\nNumero Treno: {NumeroTreno} \nParte: {OrigineZero}\nArriva: {DestinazioneZero}\n");
+            try
+            {
+                s += $"Orario: {UnixTimeStampToDateTime(Fermate[0].Programmata / 1000)}";
+            }
+            catch { }
+
+            return s;
+            /*foreach (Fermata f in Fermate)
+            {
+                try
+                {
+                    Console.WriteLine($"Stazione:{f.Stazione}");
+                    Console.WriteLine($"Ritardo Arrivo: {f.RitardoArrivo}");
+                    Console.WriteLine($"Ritardo Partenza: {f.RitardoPartenza}");
+                    System.DateTime dat_Time = UnixTimeStampToDateTime((long)f.Arrivo_Teorico / 1000);
+                    string print_the_Date = dat_Time.ToShortDateString() + " " + dat_Time.ToShortTimeString();
+                    Console.WriteLine($"ArrivoTeorico: {print_the_Date}");
+
+                    dat_Time = UnixTimeStampToDateTime((long)f.ArrivoReale / 1000);
+                    print_the_Date = dat_Time.ToShortDateString() + " " + dat_Time.ToShortTimeString();
+                    Console.WriteLine($"ArrivoReale: {print_the_Date}");
+
+
+
+                    dat_Time = UnixTimeStampToDateTime((long)f.Partenza_Teorica / 1000);
+                    print_the_Date = dat_Time.ToShortDateString() + " " + dat_Time.ToShortTimeString();
+                    Console.WriteLine($"Partenza Teoria: {print_the_Date}");
+
+                    dat_Time = UnixTimeStampToDateTime((long)f.PartenzaReale / 1000);
+                    print_the_Date = dat_Time.ToShortDateString() + " " + dat_Time.ToShortTimeString();
+                    Console.WriteLine($"PartenzaReale: {print_the_Date}");
+
+                    
+                    
+
+                }
+                catch (Exception ex) { Debug.WriteLine(ex); }
+                Console.WriteLine("\n");
+            }
+            */
+            // Aggiungi qui altri campi che vuoi visualizzare
+        }
+    }
+}
---
+++ ZIOPEDROAPI/Controllers/BetInfoController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Security;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Net.Http;
+using MySqlX.XDevAPI;
+namespace ZIOPEDROAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BetInfoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BetInfoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet(Name = "BetsController")]
+        /*public string Get()
+        {
+            MySqlConnection sql = new MySqlConnection();
+            sql.ConnectionString = "Server=localhost;Database=LudoTrain;Uid=user;Pwd=user123;";
+            sql.Open();
+            MySqlCommand cmd = sql.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Treno";
+            List<Bet> bets= new List<Bet>();
+            MySqlDataReader reader=cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                bets.Add(new Bet()
+                {
+                    IdTrain = reader.GetFieldValue<int>("idTreno"),
+                    TimeStamp = reader.GetFieldValue<DateTime>("TimeStamp"),
+                    CodiceTreno=reader.GetFieldValue<int>("codiceTreno")
+                }) ;
+            }
+            if (bets.Count > 0)
+            {
+                return JsonConvert.SerializeObject(bets);
+            }
+            else
+            {
+                Response.StatusCode = 100;
+                return "";
+            }
+
+
+        }*/
+        public string Get(int id)
+        {
+            string s;
+            
+            return JsonConvert.SerializeObject(RicercaTreno(id.ToString()));
+        }
+        static Treno PrintTrain(string trainCode, string stationCode, long timestamp)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = string.Format("http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/andamentoTreno/{1}/{0}/{2}", stationCode, trainCode, timestamp.ToString());
+
+                var task = Task.Run(() => client.GetAsync(url));
+                task.Wait();
+                HttpResponseMessage response =task.Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var task2=Task.Run(() => response.Content.ReadAsStringAsync());
+                    task2.Wait();
+                    string json =task2.Result;
+                    Treno trainData;
+                    // Deserializza il JSON in un oggetto TrainData
+                    trainData = Newtonsoft.Json.JsonConvert.DeserializeObject<Treno>(json);
+                    Newtonsoft.Json.JsonSerializer writer = new Newtonsoft.Json.JsonSerializer();
+                    // Visualizza i dati del treno
+                    return trainData;
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Errore nella richiesta: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Si è verificato un errore: " + ex.Message);
+            }
+            return null;
+        }
+
+        static Treno RicercaTreno(string codiceTreno)
+        {
+            string url = string.Format("http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/{0}", codiceTreno);
+            using (HttpClient client1 = new HttpClient())
+            {
+                var task = Task.Run(() => client1.GetAsync(url));
+                task.Wait();
+                HttpResponseMessage response = task.Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var task2 = Task.Run(() => response.Content.ReadAsStringAsync());
+                    task2.Wait();
+                    string trainData = task2.Result;
+
+                    try
+                    {
+                        string traindInfo = trainData.Split('|')[1];
+                        return PrintTrain(traindInfo.Split('-')[1].Trim(), traindInfo.Split('-')[0].Trim(), long.Parse(traindInfo.Split('-')[2].Trim()));
+                    }
+                    catch { }
+
+                }
+                else
+                {
+                    Console.WriteLine("Errore nella richiesta: " + response.StatusCode);
+                }
+            }
+            return null;
+        }
+    }
+}
